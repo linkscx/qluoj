4d4f9b89d8ca7cf9a99c8807f759673f2e672bf3
linkscx <1098509291@qq.com>
2021-08-23 20:59:09 +0800

    update

--- views/layouts/contest.php
+++ views/layouts/contest.php
@@ -198,11 +198,6 @@ $status = $model->getRunStatus();
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; <?= Yii::$app->setting->get('ojName') ?> <?= date('Y') ?></p>
-        <p class="pull-left">
-            <?= Html::a (' 中文简体 ', '?lang=zh-CN') . '| ' .
-            Html::a (' English ', '?lang=en') ;
-            ?>
-        </p>
     </div>
 </footer>
 <?php $this->endBody() ?>
